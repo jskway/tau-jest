b7bedbd5355a7ca7866058e8ecb063ed46d5954d
jackskim <jackskim29@gmail.com>
2020-03-01 18:09:11 -0800

    Add usage of .toEqual() and .not

--- multiply.test.js
+++ multiply.test.js
@@ -9,3 +9,16 @@ describe('test multiply positive scenarios', () => {
   });
 })
 
+test('object example', () => {
+  const data = { first: 1 };
+  data['second'] = 2;
+  // .toBe() uses strict equality
+  // .toEqual() tests the value of an object
+  expect(data).toEqual({ first: 1, second: 2 });
+});
+
+test('multiply 4*3 should should not equal to 11', () => {
+  // Example of .not usage
+  expect(multiply(4, 3)).not.toBe(11);
+});
+
