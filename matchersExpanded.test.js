3e97da441bbae3795f397a786d46e6f11ea89b81
jackskim <jackskim29@gmail.com>
2020-03-01 18:26:03 -0800

    Add additional number matchers

--- matchersExpanded.test.js
+++ matchersExpanded.test.js
@@ -2,3 +2,17 @@
 test('there is pool in Liverpool', () => {
   expect('Liverpool').toMatch(/pool/);
 })
+
+const multiply = require('./multiply');
+
+describe('test multiply positive scenarios', () => {
+  test('multiply 3*2 should should equal to 6', () => {
+    expect(multiply(3, 2)).toBe(6);
+    // Additional number matchers
+    expect(multiply(3, 2)).toBeGreaterThan(5);
+    expect(multiply(3, 2)).toBeLessThan(7);
+    expect(multiply(3, 2)).toBeLessThanOrEqual(6);
+  });
+})
+
+
